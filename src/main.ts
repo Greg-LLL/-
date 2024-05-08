df79e3d753f710401c5967d6e7ed33f88e2fd230
HELLO\001 <11>
2024-05-08 13:44:33 +0800

    elementUI按需引入并配置相关插件

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import App from './App.vue'
 import router from './router'
 import pinia from './store'
 
+/* 本项目按需导入,需要在tsconfig.app.json的include里
+添加 "auto-imports.d.ts","components.d.ts",才会有elementui组件的类型
+按需导入的两个插件只有在template有效,其他地方需要自己导入
+*/
+
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
 // createApp(App).use(router).mount('#app')
 const app = createApp(App)
 app.use(router)
+// app.use(ElementPlus)
 app.use(pinia)
 app.mount('#app')
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -7,6 +7,14 @@
     <button @click="counterStore.$patch({ count: 500, age: 18 })">
       批量修改
     </button>
+    <div class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
   </div>
 </template>
 
