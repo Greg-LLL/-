e4d3dae1ce6c9130d303d87f70bfb4e6ccc9c8b3
HELLO\001 <11>
2024-05-09 15:12:17 +0800

    封装Cache以及持久化保存token

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,19 +1,26 @@
 import { accountLoginRequest } from '@/service/login/login'
 import { defineStore } from 'pinia'
 import type { IAccount } from '@/types'
+import { localCache } from '@/utils/cache'
+
+const LOGIN_TOKEN = 'login/token'
 
 const useLoginStore = defineStore('login', {
   state: () => ({
     id: '',
-    token: '',
+    token: localCache.getCache(LOGIN_TOKEN) ?? '',
     name: ''
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
+      // 获取登录信息
       const loginResult = await accountLoginRequest(account)
       this.id = loginResult.data.id
       this.token = loginResult.data.token
       this.name = loginResult.data.name
+
+      // 进行本地缓存
+      localCache.setCache(LOGIN_TOKEN, this.token)
     }
   }
 })
