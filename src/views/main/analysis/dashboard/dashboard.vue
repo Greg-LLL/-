d69c0eebcf3c0163a72f0b780bc7f587e0278392
Greg <402164923@qqq.com>
2024-05-30 14:02:31 +0800

    商品统计-头部完成

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="dashboard">
-    <h2>dashboard</h2>
+    <!-- 顶部 数字的数据展示 -->
+    <el-row :gutter="10">
+      <template v-for="item in amountList" :key="item">
+        <el-col :span="6"><CountCard v-bind="item" /></el-col
+      ></template>
+    </el-row>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import CountCard from './c-cpns/count-card/count-card.vue'
+import useAnalysisStore from '@/store/main/analysis/analysis'
+
+// 发起请求
+const analysisStore = useAnalysisStore()
+analysisStore.fetchAnalysisDataAction()
+
+// 从store获取数据
+const { amountList } = storeToRefs(analysisStore)
+</script>
 
 <style lang="less" scoped>
 .dashboard {
